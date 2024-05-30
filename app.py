c60e7a9b45332cdf78eaa88719a97a1bf5cc99e3
Raj Patel <rajv23p@gmail.com>
2024-05-30 10:56:23 -0400

    FINAL COMMIT

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request
-import sqlite3
+import sqlite3,json,pickle
 
 app = Flask(__name__)
 
@@ -31,6 +31,37 @@ def contactus():
     else:
         return render_template('contactus.html')
     
-    
+@app.route ("/check",methods = ["GET","POST"])
+def predict():
+    if request.method == "POST":
+        make = request.form.get("make")
+        model = request.form.get("model")
+        year = request.form.get("year")
+        kms_driven = request.form.get("kms_driven")
+        fuel = request.form.get("fuel")
+        registration_city = request.form.get("registration_city")
+        car_documents = request.form.get("car_documents")
+        assembly = request.form.get("assembly")
+        transmission =  request.form.get("transmission")
+        print(make,model,year,kms_driven,fuel,registration_city,car_documents,assembly,transmission)
+        with open("encdata.json","r") as file:
+            data = json.load(file)
+        mkenc = int(data["Make"][make])
+        mdlenc = int(data["Model"][model])
+        fenc = int(data["Fuel"][fuel])
+        rgctenc = int(data["Registration city"][registration_city])
+        cardcenc =  int(data["Car documents"][car_documents])
+        assenc  = int(data["Assembly"][assembly])
+        trenc = int(data["Transmission"][transmission])
+        print(mkenc,mdlenc,fenc,rgctenc,cardcenc,assenc,trenc)
+        file.close()
+        with open("model.pickle","rb") as model:
+            mlmodel = pickle.load(model)
+        res = mlmodel.predict([[int(year),int(kms_driven),mkenc,mdlenc,fenc,rgctenc,cardcenc,assenc,trenc]])    
+        print(res[0])
+        return render_template("result.html",car_price = str(int(res[0]*0.3))+" INR") 
+    else:
+        return render_template("predict.html")
+
 if __name__ =='__main__':
-    app.run()
+    app.run(host = "0.0.0.0",port = 5500)
