6992107160c4c137c67b9f2e2d0d6f961ef08c99
Raj Patel <rajv23p@gmail.com>
2024-05-30 02:54:47 -0400

    30night

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -7,9 +8,29 @@ def homepage():
     return render_template("home.html")
 
 
-@app.route('/contact')
+@app.route('/contact',methods = ["GET","POST"])
 def contactus():
-    return render_template('contactus.html')
-
+    if request.method == 'POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        country = request.form.get("country")
+        state = request.form.get("state")
+        message = request.form.get("message")
+        print(name,email,country,state,message)
+        conn = sqlite3.connect('contactus.db')
+        cur = conn.cursor()
+        cur.execute(f'''
+        INSERT INTO CONTACT VALUES(
+            "{name}",
+            "{email}","{country}",
+            "{state}","{message}"
+        )            
+        ''')
+        conn.commit()
+        return render_template("message.html")
+    else:
+        return render_template('contactus.html')
+    
+    
 if __name__ =='__main__':
     app.run()
